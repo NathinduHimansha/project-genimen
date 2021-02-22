51b10dfa5df8bbaff58d647c23c844dc1edaf9bb
bsucker98 <bmorashad@gmail.com>
2021-02-22 18:32:31 +0530

    improved text cleaning func

--- User Requested Aspect Sentiment/DataCleaning.py
+++ User Requested Aspect Sentiment/DataCleaning.py
@@ -5,6 +5,7 @@ import re
 
 
 # function for data cleaning
+
 def data_clean(text):
     list_punctuation = set(string.punctuation)  # list of punctuations
     list_numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]  # list of numbers
@@ -36,3 +37,18 @@ def data_clean(text):
             optimized_text.append(word)
 
     return optimized_text
+
+def de_emojify(text):
+    return re.sub("(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])", "", text)
+
+def clean_text(text):
+    text = text.lower()
+    html_filter = re.sub('<[^>]*>', ' ', text)
+    reduntant_space_filter = re.sub('\s{2,}', ' ', html_filter)
+    non_alphabet_filter = re.sub('(?!\s)[\W|\d]', '', html_filter)
+    emoji_filter = de_emojify(non_alphabet_filter)
+    non_ascii_filter = emoji_filter.encode("ascii", errors='ignore').decode()
+    punctuation_filter = ' '.join(word.strip(string.punctuation) for word in non_ascii_filter.split())
+    return punctuation_filter
+
+print(clean_text('<html> <span> Hello I am Nathindu (0_0) from Mass @456hs i am 21 years old I like super cool curved% phone its amazing.! 10FFFFF16 hindi 😛 Ståle?'))
