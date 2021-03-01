7de184882ce2ad9a1eed86cc412c191a04c09757
bsucker98 <bmorashad@gmail.com>
2021-03-01 18:15:25 +0530

    minor parameter optimization

--- User Requested Aspect Sentiment/uras/model/naive_bayes_sentiment_predictor.py
+++ User Requested Aspect Sentiment/uras/model/naive_bayes_sentiment_predictor.py
@@ -14,7 +14,8 @@ from imblearn.over_sampling import SMOTE
 def naive_bayes_prediction(reviews_text, polarity_score):
     reviews_train, reviews_test, polarity_train, polarity_test = train_test_split(reviews_text, polarity_score,
                                                                                   test_size=0.2)
-    vectorizer = CountVectorizer(max_features=1000, binary=True)
+    vectorizer = CountVectorizer(max_df=0.9, ngram_range=(1,1), binary=True)
+    #  vectorizer = CountVectorizer(binary=True)
     reviews_train_vector = vectorizer.fit_transform(reviews_train)
 
     # data balancing
@@ -31,17 +32,17 @@ def naive_bayes_prediction(reviews_text, polarity_score):
     polarity_prediction = naive_bayes_model.predict(reviews_test_vector)
 
     # save the model to disk
-    filename = 'naive_bayes_model.sav'
+    filename = 'model/trained/naive_bayes_model.sav'
     pickle.dump(naive_bayes_model, open(filename, 'wb'))
 
     print("\nPREDICTED ACCURACY: {:.2f}%".format(accuracy_score(polarity_test, polarity_prediction) * 100))
     print("\nF1 SCORE: {:.2f}".format(f1_score(polarity_test, polarity_prediction, average="micro")))
+    print("\nF1 SCORE: " + str(f1_score(polarity_test, polarity_prediction, average=None)))
     print("\nCONFUSION MATRIX: \n", confusion_matrix(polarity_test, polarity_prediction))
 
-
-pos = pd.read_csv('/Users/nathinduhimansha/User-Data/IIT/2nd_Year/2nd_Semester/SDGP/project-genimen/User Requested Aspect Sentiment/uras/data/pos.csv').dropna()[:10000]
-neg = pd.read_csv('/Users/nathinduhimansha/User-Data/IIT/2nd_Year/2nd_Semester/SDGP/project-genimen/User Requested Aspect Sentiment/uras/data/neg.csv').dropna()[:10000]
-neu = pd.read_csv('/Users/nathinduhimansha/User-Data/IIT/2nd_Year/2nd_Semester/SDGP/project-genimen/User Requested Aspect Sentiment/uras/data/neu.csv').dropna()[:10000]
+pos = pd.read_csv('data/splitted/pos.csv').dropna()[:10000]
+neg = pd.read_csv('data/splitted/neg.csv').dropna()[:10000]
+neu = pd.read_csv('data/splitted/neu.csv').dropna()[:10000]
 
 df = pos.append(neg).append(neu)
 df = df.sample(frac=1)
