53c6a9f3f891b4f607c206bcfc5c51d66b48a508
Malith404 <malith.2018402@iit.ac.lk>
2021-03-13 11:47:16 +0530

    frequency-based UI heading added

---
+++ web-frontend/src/components/heading/Frequency-based-heading.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import logo from '../assets/geniman_logo_new.png'
+import './heading.css'
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Frequency_based_heading extends React.Component {
+    render() {
+        return (
+            <div className="main-container">
+
+                <div className="header">
+                    <div className="header-left-split">
+                        <img src={logo} className="logo" alt="logo" />
+                    </div>
+                    <div className="header-right-split">
+
+                        <Navbar variant="light">
+                            <Nav className="navElements">
+                                <Nav.Link href="#home">Home</Nav.Link>
+                                <Nav.Link href="#about" style={{ marginLeft: '170px' }}>About</Nav.Link>
+                                <Nav.Link href="#developers" style={{ marginLeft: '40%' }}>Developers</Nav.Link>
+                                <Nav.Link href="#help" style={{ marginLeft: '38%' }}>Help</Nav.Link>
+                            </Nav>
+
+                        </Navbar>
+
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Frequency_based_heading;
