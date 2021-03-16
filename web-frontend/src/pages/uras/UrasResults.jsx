643b8bd3b1b6de2573afbb87ced82dd269b6bbbc
nathindu <nathinduhimansha@gmail.com>
2021-03-16 14:59:48 +0530

    uras results page

---
+++ web-frontend/src/pages/uras/UrasResults.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UrasResults extends Component {
+    render() {
+        return (
+            <div>
+                gdfgfg
+            </div>
+        )
+    }
+}
+
+export default UrasResults
