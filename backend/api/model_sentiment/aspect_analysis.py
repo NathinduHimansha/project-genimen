c41787fed34da87b3485c8d9a428ed3fa8628f0a
Andrew <andrewdesilvasl@gmail.com>
2021-03-20 23:03:27 +0530

    fixed path error & added api to routes

--- backend/api/model_sentiment/aspect_analysis.py
+++ backend/api/model_sentiment/aspect_analysis.py
@@ -1,12 +1,12 @@
-from backend.analytics.model_specific.smartphone_model.phone_model import Model
+from analytics.model_specific.smartphone_model.phone_model import Model
 import pickle
-from backend.analytics.sentiment_analysis.preprocessing.clean_data import clean_text
+from analytics.sentiment_analysis.preprocessing.clean_data import clean_text
 
 def get_sentiment(sentence_list):
     model = pickle.load(
-        open("../../analytics/sentiment_analysis/model/trained_model/sgd_model/sgd_model.pickle", 'rb'))
+        open("analytics/sentiment_analysis/model/trained_model/sgd_model/sgd_model.pickle", 'rb'))
     vc = pickle.load(
-        open("../../analytics/sentiment_analysis/model/trained_model/sgd_model/vectorizer.pickle", 'rb'))
+        open("analytics/sentiment_analysis/model/trained_model/sgd_model/vectorizer.pickle", 'rb'))
     cleaned_sentences = list(map(clean_text, sentence_list))
     bow = vc.transform(cleaned_sentences)
     return model.predict(bow)
