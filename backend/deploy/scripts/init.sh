4cea868c99a34061200a18607ebacd9ee40a9c69
bsucker98 <bmorashad@gmail.com>
2021-03-22 01:11:09 +0530

    aws codedeploy config & scripts added

---
+++ backend/deploy/scripts/init.sh
@@ -0,0 +1,10 @@
+cd /home/ubuntu/sdgp-backend
+aws s3 cp s3://sdgp-bucket/backend-api/dev/env/.env ./api
+mkdir -p ./analytics/
+aws s3 sync s3://sdgp-bucket/phone_reviews data/phone_reviews
+cp -r /home/ubuntu/data ./analytics
+cat ./api/.env
+
+cp ./deploy/systemd/sdgpapp.service /etc/systemd/system/ 
+sudo service systemctl daemon-reload
+cp ./deploy/nginx/sdgpapp /etc/nginx/sites-enabled
