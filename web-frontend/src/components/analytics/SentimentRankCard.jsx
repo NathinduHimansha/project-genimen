b7c380015c153b4af0d63614f707b14fb5d0f59a
bsucker98 <bmorashad@gmail.com>
2021-03-24 00:13:21 +0530

    sentiment rank card added

---
+++ web-frontend/src/components/analytics/SentimentRankCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import smily from '../../assests/smily-emoji.png';
+import angry from '../../assests/angry-emoji.png';
+
+const SentimentRankCard = (props) => {
+  let { polarityPerc, label, description, polarity } = props;
+  const color = polarity == 'pos' ? 'var(--pos-green)' : 'var(--neg-red)';
+  const polarityPercLabel = polarity == 'pos' ? '+' + polarityPerc + '%' : polarityPerc + '%';
+
+  return (
+    <div>
+      <div
+        className={`sentiment-rank-card ${
+          polarity == 'pos' ? 'sentiment-rank-card--pos' : 'sentiment-rank-card--neg'
+        }`}
+      >
+        <div className="sentiment-det">
+          <h1 className="sentiment-det-label heading1 -medium -no-margin ">{label}</h1>
+          <span className="sentiment-det-description">{description}</span>
+          <h1 className="heading1 sentiment-det-polarity-perc -light -no-margin">
+            {polarityPercLabel}
+          </h1>
+        </div>
+        <div className="sentiment-emoji">
+          {polarity == 'pos' ? <img src={smily} /> : <img src={angry} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SentimentRankCard;
