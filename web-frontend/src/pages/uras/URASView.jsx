ef61bd293a7fc3abfcf2f1cafb8cccd4b2f9f09c
Nathindu <nathinduhimansha@gmail.com>
2021-03-25 23:53:38 +0530

    toast notification added

--- web-frontend/src/pages/uras/URASView.jsx
+++ web-frontend/src/pages/uras/URASView.jsx
@@ -5,8 +5,12 @@ import Button from '../../components/buttons/Button';
 import UrasFeaturesInput from './UrasFeaturesInput';
 
 import {analyseFeatures} from '../../services/uras-service';
+import { ToastContainer, toast,Zoom,Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 import './uras.css';
+import ReactToast from './ReactToast';
 
 class UrasView extends Component {
 
@@ -15,25 +19,32 @@ class UrasView extends Component {
         this.state = {
              loading:false,//button loading state
              data :[], //stores received data from backend
-             selectedFeatures:{} //stores selected features
+             selectedFeatures:{}, //stores selected features
+             toastVisibility:false
         }
     }
 
     //handles the http request and routes the user
-    getRequestedData = ()=>{
+    getRequestedData = (event)=>{
         this.setButtonState(true),
+        
        
         //http request handling
         // analyseFeatures(this.state.selectedFeatures).then((response) => {
         analyseFeatures().then((response) => {
-            this.setState({data:response.data}),
-            this.setButtonState(false),
-            this.routePage()
+            this.setState({data:response.data,toastVisibility:true}),
+            this.setButtonState(true)
+            event.preventDefault();
+            
+            // this.routePage()
         })
         .catch(error =>{
-            console.log("ERROR LOGGED IN UrasView getRequstedData: ", error)
+            if(error){
+                
+            }
         })
     }
+  
 
     //change the loading button state
     setButtonState = (state)=>{
@@ -56,11 +67,17 @@ class UrasView extends Component {
             selectedFeatures: {...prev.selectedFeatures,[feature]: type}
         }))
     }
+    
+    
 
 
     render() {
         return (
             <div style={{ margin: '10% 10%' }}>
+
+{this.state.toastVisibility==true?
+               <ReactToast dataFromParent="a" type="error" message="YAAAAAAAAAAY wadaaaaaaa" close={true} timeout={5000}/> : null}
+
                 <div className="-mb-40">
                     <FancyHeading heading="SELECT FEATURES TO ANALYSE" />
                 </div>
@@ -70,7 +87,7 @@ class UrasView extends Component {
                 </div>
              
                 <div className="-flex -flex-center -flex-middle -mt-40">
-                    <Button onClick={()=>this.getRequestedData()} iconSrc={search} loading={this.state.loading}>
+                    <Button onClick={()=>this.getRequestedData(event)} iconSrc={search} loading={this.state.loading}>
                         Start Analysing
                     </Button>    
                 </div>   
