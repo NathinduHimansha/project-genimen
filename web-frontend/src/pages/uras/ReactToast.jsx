4ef765f3174e3945fe1c76c23ffd4c614779462b
Nathindu <nathinduhimansha@gmail.com>
2021-03-26 23:42:46 +0530

    toast component updated

--- web-frontend/src/pages/uras/ReactToast.jsx
+++ web-frontend/src/pages/uras/ReactToast.jsx
@@ -1,59 +1,64 @@
 import React from 'react'
-import { ToastProvider, useToasts } from 'react-toast-notifications';
+import {useToasts } from 'react-toast-notifications';
 
 export default function ReactToast(props) {
-    const userType = props.type;
-    const userMessage = props.message;
-    const userClose = props.close;
-    const userTime = props.timeout;
+  const userType = props.type;
+  const userMessage = props.message;
+  const userClose = props.close;
+  const userTime = props.timeout;
     
-    const { addToast } = useToasts();
-    console.log("func cmop")
-    console.log(userClose)
-    console.log(userTime)
+  const { addToast } = useToasts();
 
+  function SwitchCase(props) {
+    switch(props.value) {
+      case 'success':
+        addToast(userMessage, 
+          {
+            appearance: userType,
+            id: 'toast-success',
+            autoDismiss:userClose,
+            autoDismissTimeout:userTime,
+          }
+        );
 
-
-    function SwitchCase(props) {
-        switch(props.value) {
-          case 'success':
-            addToast(userMessage , {
-        appearance: userType,
-        id: 'toast-success',
-        autoDismiss:userClose,
-        autoDismissTimeout:userTime,
-      });
-          case 'error':
-            addToast(userMessage , {
-        appearance: userType,
-        id: 'toast-error',
-        autoDismiss:userClose,
-        autoDismissTimeout:userTime,
-      });
+      case 'error':
+        addToast(userMessage, 
+          {
+            appearance: userType,
+            id: 'toast-error',
+            autoDismiss:userClose,
+            autoDismissTimeout:userTime,
+          }
+        );
 
       case 'warning':
-            addToast(userMessage , {
-        appearance: userType,
-        id: 'toast-warning',
-        autoDismiss:userClose,
-        autoDismissTimeout:userTime,
-      });
+        addToast(userMessage, 
+          {
+            appearance: userType,
+            id: 'toast-warning',
+            autoDismiss:userClose,
+            autoDismissTimeout:userTime,
+          }
+        );
 
       case 'info':
-            addToast(userMessage , {
-        appearance: userType,
-        id: 'toast-info',
-        autoDismiss:userClose,
-        autoDismissTimeout:userTime,
-      });
-          default:
-            return '';
-        }}
+        addToast(userMessage , 
+          {
+            appearance: userType,
+            id: 'toast-info',
+            autoDismiss:userClose,
+            autoDismissTimeout:userTime,
+          }
+        );
+
+        default:
+          return '';
+        }
+    }
       
-    
     return (
-        <div className="container">
+      <div className="container">
         <SwitchCase value={userType} />
-    </div>
+     </div>
     )
 }
