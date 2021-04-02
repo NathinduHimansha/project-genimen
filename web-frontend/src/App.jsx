86dd77d26ca51a1e6614b6a5b07a7042b29fd128
Nathindu <nathinduhimansha@gmail.com>
2021-04-03 01:48:33 +0530

    scroll re added

--- web-frontend/src/App.jsx
+++ web-frontend/src/App.jsx
@@ -11,41 +11,7 @@ import Examples from './pages/experiment/Examples';
 import { ToastProvider, useToasts } from 'react-toast-notifications';
 
 import HomePage from './pages/index/HomePage';
-<<<<<<< HEAD
 
-import UrasUserInputView from './pages/uras/UrasUserInputView';
-import ScrollUp from './components/scrollupbutton/ScrollUp';
-import UrasInputResults from './pages/uras/UrasInputResults';
-import testcard from './pages/experiment/testcard';
-
-
-const routes = [
-  {
-    title: 'Home',
-    icon: home,
-    subMenu: [],
-    path: '/home',
-  },
-  {
-    title: 'About',
-    icon: about,
-    subMenu: [],
-    path: '/about',
-  },
-  {
-    title: 'Analytics',
-    icon: search,
-    subMenu: [
-      { title: 'Sentiment by Features ', path: '/analytics/uras' },
-      { title: 'Sentiment by Models', path: '/analytics/pssa' },
-      { title: 'Trending Features', path: '/analytics/exkey' },
-    ],
-    path: '/analytics',
-  },
-  
-];
-=======
-import ScrollUp from './components/ScrollUpButton/ScrollUp';
 import UrasUserInputView from './pages/uras/UrasUserInputView';
 import UrasInputResults from './pages/uras/UrasInputResults';
 import URASViewAlt from './pages/uras/URASViewAlt';
@@ -54,7 +20,7 @@ import Store from './components/sate_management/GlobalStore';
 import { isLoggedIn } from './common/utils';
 import PssaView from './pages/pssa2/pssaView';
 import PssaResults from './pages/pssa2/pssaResults';
->>>>>>> 344dd671489967970941352e9243e5df61855177
+import ScrollUp from './components/scrollupbutton/ScrollUp';
 
 function App() {
   const [state, dispatch] = useContext(Context);
@@ -85,6 +51,8 @@ function App() {
           <Route path="/examples" component={Examples} />
           <Route exact path="/" component={() => <Redirect to="/home" />} />
         </Switch>
+
+        <ScrollUp />
       </div>
     );
   };
@@ -93,34 +61,9 @@ function App() {
       <Router>
         <div className="App">
           <Switch>
-<<<<<<< HEAD
-            <Route exact path="/" component={HomePage}>
-              <Redirect to="/index" />
-            </Route>
-            <Route path="/home" component={HomePage} />
-            <Route path="/index" component={HomePage} />
-            <Route path="/about" component={Login} />
-
-            <Route exact path="/analytics/uras" component={UrasUserInputView} />
-            <Route exact path="/analytics/uras/results" component={UrasInputResults} />
-
-            <Route path="/analytics/exkey" component={EXKEY} />
-
-            <Route path="/analytics/pssa" component={Home} />
-          
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-
-            <Route path="/examples" component={Examples} />
-            <Route path="/exkey" component={EXKEY} />
-            <Route path="/pssa" component={Home} />
-            <Route path="/testcard" component={testcard} />
-=======
             <Route exact path="/home" component={HomePage} />
             <Route component={RoutesWNav} />
->>>>>>> 344dd671489967970941352e9243e5df61855177
           </Switch>
-          <ScrollUp />
         </div>
       </Router>
     </ToastProvider>
