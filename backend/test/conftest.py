f382cedd54460b0914d46fb3ca88585dd3da20ea
bsucker98 <bmorashad@gmail.com>
2021-04-05 07:56:23 +0530

    test case minor improvement

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -18,8 +18,9 @@ def db_client():
     app = create_app('test')
     app.config["TESTING"] = True
     with app.test_client() as client:
+        with app.app_context() as ctx:
+            ctx.push()
         _db = db.get_db()
         for collection in _db.list_collection_names():
             _db.drop_collection(collection)
-        client.get()
         yield client
