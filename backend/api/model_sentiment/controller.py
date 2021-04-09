044b7c6c0d667ed2e1318e2b08b3e3d37114fe05
Tharindu <andrewdesilvasl@gmail.com>
2021-04-09 10:47:37 +0530

    added sending model list from back to front

--- backend/api/model_sentiment/controller.py
+++ backend/api/model_sentiment/controller.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, Blueprint
+
+from analytics.model_specific.model_lexicon.model_lexicon import available_models
 from api.common.utils import createSuccessResponse, createErrResponse
 from api.model_sentiment.service import get_model_sentiment
 
@@ -12,6 +14,14 @@ def handle_model_sentiment(model_name):
     if request.method == 'GET':
         return createSuccessResponse(get_model_sentiment(model_name).__dict__)
     else:
+        return createErrResponse("selected smartphone model is currently not available!")
+
+@model_sentiment.route('/ModelSentiment/getmodels', methods=['GET'])
+def get_model_list():
+    if request.method == 'GET':
+        return createSuccessResponse(available_models)
+    else:
         return createErrResponse()
+
 
 
