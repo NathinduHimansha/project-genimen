0fc922b6703e826b9a06fd8ef7d556d60d48735b
Tharindu <andrewdesilvasl@gmail.com>
2021-04-10 19:16:12 +0530

    minor improvements

--- backend/api/model_sentiment/controller.py
+++ backend/api/model_sentiment/controller.py
@@ -1,20 +1,24 @@
 from flask import Flask, request, Blueprint
 
-from analytics.model_specific.model_lexicon.model_lexicon import available_models
+from analytics.model_specific.model_lexicon.model_lexicon import available_models, MODELS
 from api.common.utils import createSuccessResponse, createErrResponse
 from api.model_sentiment.service import get_model_sentiment
 
 
 model_sentiment = Blueprint('ModelSentiment', __name__, url_prefix='/api')
 
+# Checking the model is available
+def is_model_valid(model_name):
+    if model_name in MODELS:
+        return True
 
 @model_sentiment.route('/ModelSentiment/<string:model_name>', methods=['GET'])
 def handle_model_sentiment(model_name):
     model_name = str(model_name)
+    if not is_model_valid(model_name):
+        return createErrResponse("smartphone model not available !")
     if request.method == 'GET':
         return createSuccessResponse(get_model_sentiment(model_name).__dict__)
-    else:
-        return createErrResponse("selected smartphone model is currently not available!")
 
 @model_sentiment.route('/ModelSentiment/getmodels', methods=['GET'])
 def get_model_list():
