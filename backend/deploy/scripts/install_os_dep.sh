3cbbd6637c7e6cb30fcf3f374d1a9b76ff2ea16f
bsucker98 <bmorashad@gmail.com>
2021-04-14 17:34:49 +0530

    ci/cd bug fix

--- backend/deploy/scripts/install_os_dep.sh
+++ backend/deploy/scripts/install_os_dep.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install python3-pip nginx gunicorn awscli -y python3-flask
+sudo apt install python3-pip nginx gunicorn awscli python3-flask -y 
 pip3 install --upgrade awscli
