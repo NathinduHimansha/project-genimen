528e53f44bb64b1b9bf2c18c1243dd9842b3cad3
bsucker98 <bmorashad@gmail.com>
2021-04-15 17:18:58 +0530

    signup and login improvements

--- web-frontend/src/pages/login/Login.jsx
+++ web-frontend/src/pages/login/Login.jsx
@@ -4,9 +4,10 @@ import Button from '../../components/buttons/Button';
 import logo from '../../assests/Geniman.png';
 import banner from '../../assests/LineChartBanner.png';
 import './login.css';
-import { NavLink, useHistory } from 'react-router-dom';
-import { isPasswordValid, saveToken } from '../../common/utils';
+import { NavLink, useHistory, useLocation } from 'react-router-dom';
+import { isEmailValid, isPasswordValid, logIn, saveToken } from '../../common/utils';
 import { ToastProvider, useToasts } from 'react-toast-notifications';
+import { login } from '../../services/auth-service';
 
 const Login = (props) => {
   const [passwordError, setPasswordError] = useState(false);
@@ -18,6 +19,7 @@ const Login = (props) => {
   const [loading, setLoading] = useState(false);
   const { addToast } = useToasts();
   const history = useHistory();
+  let path = useLocation().pathname;
 
   const [state, dispatch] = useContext(Context);
   const onPasswordFocus = () => {
@@ -46,17 +48,43 @@ const Login = (props) => {
   const onLogin = () => {
     onUsernameBlur(username);
     onPasswordBlur(password);
-    dispatch({ type: 'LOGIN' });
-    saveToken('taifkdf23reraerdafkljafkdl4er');
+    // dispatch({ type: 'LOGIN' });
+    // saveToken('taifkdf23reraerdafkljafkdl4er');
     const isFormValid = isInputsValid(password, username);
     if (isFormValid) {
       setLoading(true);
-      setTimeout(() => {
-        addToast('Successfully Logged in', { appearance: 'success', id: 'login-success' });
-        dispatch({ type: 'TOGGLE_LOGIN' });
-        history.push({ pathname: '/signup' });
-        setLoading(false);
-      }, 3000);
+      const credentials = {};
+      credentials['password'] = password;
+      if (isEmailValid(username)) {
+        credentials['email'] = username;
+      } else {
+        credentials['username'] = username;
+      }
+      login(credentials)
+        .then((res) => {
+          if (res.data.status == 1) {
+            logIn(res.data.token);
+            dispatch({ type: 'LOGIN' });
+            addToast('Successfully Logged in', { appearance: 'success', id: 'login-success' });
+            if (pathname.inclues('login')) {
+              history.push('/analytics');
+            }
+          } else {
+            addToast(res.data.message, {
+              appearance: 'error',
+              id: 'login-error',
+            });
+            setLoading(false);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          addToast('Something went wront, please try again...', {
+            appearance: 'error',
+            id: 'login-error',
+          });
+          setLoading(false);
+        });
     } else {
       addToast('Invalid username/password, please try again ', {
         appearance: 'error',
@@ -99,7 +127,7 @@ const Login = (props) => {
                     }}
                     onChange={(event) => {
                       setUsername(event.target.value);
-                      onUsernameBlur(event.target.value);
+                      // onUsernameBlur(event.target.value);
                     }}
                     onFocus={onUsernameFocus}
                   />
@@ -132,7 +160,7 @@ const Login = (props) => {
                     }}
                     onChange={(event) => {
                       setPassword(event.target.value);
-                      onPasswordBlur(event.target.value);
+                      // onPasswordBlur(event.target.value);
                     }}
                     onFocus={onPasswordFocus}
                   />
