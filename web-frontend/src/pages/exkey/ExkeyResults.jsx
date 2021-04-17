bf69d77c990d665f7b539a14887f50afbf4573f6
Malith404 <malith.2018402@iit.ac.lk>
2021-04-17 10:21:19 +0530

    Exkey class components converted to functional

--- web-frontend/src/pages/exkey/ExkeyResults.jsx
+++ web-frontend/src/pages/exkey/ExkeyResults.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import IconHeading from '../../components/text/IconHeading';
 import './exkey.css';
@@ -7,67 +7,91 @@ import TreeMap from '../../components/graphs/TreeMap';
 import lightBulb from '../../assests/tip_bulb.png';
 import FancyHeading from '../../components/text/FancyHeading';
 import fire from '../../assests/fire.png';
-// import { useHistory } from 'react-router';
+import { Redirect, useHistory } from 'react-router';
 import fromParentOnly from '../../pages/wrappers/FromParentOnly';
 
-class ExkeyResults extends React.Component {
-  constructor(props) {
-    super(props);
+const ExkeyResults = () => {
+  const history = useHistory();
+  const [pageLoading, setPageLoading] = useState(false);
 
-    this.state = {
-      trendingFeatures: [], //trending keywords array (card left)
-      otherKeywordsList: [], //other keywords array (card right)
-    };
-  }
+  useEffect(() => {
+    setPageLoading(true);
+    setTimeout(() => {
+      setPageLoading(false);
+    }, 600);
+  }, []);
 
-  componentDidMount() {
-    const { history } = this.props;
+  // constructor(props) {
+  //   super(props);
 
-    const trendingFeatures = history.location.state;
-    this.state.trendingFeatures = trendingFeatures;
-    this.setState({ trendingFeatures });
+  //   this.state = {
+  //     trendingFeatures: [], //trending keywords array (card left)
+  //     otherKeywordsList: [], //other keywords array (card right)
+  //   };
+  // }
 
-    // const { history } = this.props;
+  // componentDidMount() {
+  //   const { history } = this.props;
 
-    // const trendingFeatures = history.location.state;
-    console.log(trendingFeatures);
-    // this.state.trendingFeatures = trendingFeatures;
-    // this.setState({ trendingFeatures, isLoading: false });
-  }
+  //   const trendingFeatures = history.location.stateTrending;
+  //   this.state.trendingFeatures = trendingFeatures;
+  //   this.setState({ trendingFeatures });
 
-  render() {
-    const { trendingFeatures } = this.state;
+  //   const otherKeywords = history.location.stateOtherKeywords;
+  //   this.state.otherKeywordsList = otherKeywords;
+  //   this.setState({ otherKeywords });
 
-    return (
-      <div className="main-body">
-        {/*align the header according to the window sizes*/}
-        <div className="heading_align">
-          <div className="app-heading-header content-padding -flex -flex-col">
-            <div className="-mb-30">
-              {/*showing the current location of the page when it navigates from one page to another*/}
-              <NavLink to="/analytics/exkey" className="-text-decoration-none">
-                <IconHeading size="extra-small" iconUrl="var(--arrow-back-icon)">
-                  <h4 className="heading4 -no-margin">
-                    <span className="header-go-back">Back</span>
-                  </h4>
-                </IconHeading>
-              </NavLink>
-            </div>
-            {/*main topic of the heading*/}
-            <h2 className="fancy-heading -no-margin">LOOK WHAT PEOPLE ARE TALKING OF PHONES...</h2>
+  //   console.log(this.state.trendingFeatures);
+  //   console.log(this.state.otherKeywordsList);
+  // }
+
+  // const { trendingFeatures } = this.state;
+  // const history = useHistory();
+
+  const trendingFeatures = history.location.stateTrending;
+  console.log(trendingFeatures);
+
+  const exkeyDataotherKeyword = history.location.stateOtherKeywords;
+  console.log(exkeyDataotherKeyword);
+
+  return (
+    <div className="main-body">
+      {/*align the header according to the window sizes*/}
+      <div className="heading_align">
+        <div className="app-heading-header content-padding -flex -flex-col">
+          <div className="-mb-30">
+            {/*showing the current location of the page when it navigates from one page to another*/}
+            <NavLink to="/analytics/exkey" className="-text-decoration-none">
+              <IconHeading size="extra-small" iconUrl="var(--arrow-back-icon)">
+                <h4 className="heading4 -no-margin">
+                  <span className="header-go-back">Back</span>
+                </h4>
+              </IconHeading>
+            </NavLink>
           </div>
-          <div className=" -mt-60 -mb-90 content-padding">
-            <FancyHeading decoratorClassName="fancy-heading2-decorator">
-              <h2 className="heading2 -medium -no-margin heading2-sep-margin">
-                {/*subtopic of the heading*/}
-                Smartphone Feature Trendz
-              </h2>
-            </FancyHeading>
-            {/*line seperator after the subtopic*/}
-            <hr className="heading-sep" />
-          </div>
+          {/*main topic of the heading*/}
+          <h2 className="fancy-heading -no-margin">RESULTS</h2>
         </div>
+        <div className=" -mt-60 -mb-90 content-padding">
+          <FancyHeading decoratorClassName="fancy-heading2-decorator">
+            <h2 className="heading2 -medium -no-margin heading2-sep-margin">
+              {/*subtopic of the heading*/}
+              Trending Smartphone Features
+            </h2>
+          </FancyHeading>
+          {/*line seperator after the subtopic*/}
+          <hr className="heading-sep" />
+        </div>
+      </div>
 
+      {pageLoading ? (
+        <div className="-flex -full-width -mb-70 -mt-40">
+          <div
+            style={{ marginLeft: '50%', marginTop: '10%' }}
+            className=" spinner spinner-medium spinner-accent"
+          ></div>
+        </div>
+      ) : (
         <div className="body-split">
           <div className="-mt-60">
             <div className="analytics-container cards-split -mt-40">
@@ -91,6 +115,7 @@ class ExkeyResults extends React.Component {
                     </h3>
                   </div>
                 </div>
+
                 {/*showing the bargrpah to the user after the relavent timecount mentioned above inside the button action*/}
                 <div className="frequency-bars">
                   {trendingFeatures.map((item, i) => (
@@ -102,6 +127,7 @@ class ExkeyResults extends React.Component {
               {/*starting of the card right elements*/}
 
               {/*hidding the right card at the begining of the exkey page which represents the treemap*/}
+
               <div className="card-right">
                 {/*trend description which is dispplayed on the right side of the begining of the exkey page*/}
 
@@ -135,18 +161,18 @@ class ExkeyResults extends React.Component {
                     </div>
                   </div>
 
-                  {/*Treemap which is imported from the gra[h folder which represents the similar keywords*/}
+                  {/*Treemap which is imported from the graph folder which represents the similar keywords*/}
                   <div className="treeMap_align">
-                    <TreeMap />
+                    <TreeMap data={exkeyDataotherKeyword} />
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
-export default fromParentOnly(ExkeyResults);
+export default ExkeyResults;
