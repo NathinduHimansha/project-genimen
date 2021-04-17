a9a7eef4775a382cf0e6acaccc5c7d02af62fe25
Malith404 <malith.2018402@iit.ac.lk>
2021-04-17 12:23:28 +0530

    Exkey page loading and animation added

--- web-frontend/src/pages/exkey/Exkey.jsx
+++ web-frontend/src/pages/exkey/Exkey.jsx
@@ -41,7 +41,7 @@ const Exkey = () => {
 
             history.push({
               pathname: '/analytics/exkey/results',
-              state: { trendList: trendingList, otherKeywords: otherKeywordList },
+              state: { stateTrending: trendingList, stateOtherKeywords: otherKeywordList },
             });
           } else {
             addToast('Something went wrong with the request!, please try again...', {
@@ -62,84 +62,82 @@ const Exkey = () => {
   };
 
   return (
-    <div className="main-body">
-      {/*align the header according to the window sizes*/}
-      <div className="heading_align">
-        <div className="app-heading-header content-padding -flex -flex-col">
-          <div className="-mb-30">
-            {/*showing the current location of the page when it navigates from one page to another*/}
-            <CurrentLocation></CurrentLocation>
+    <div className="navbar-page-container -mb-40" style={{ margin: '0%' }}>
+      <div className="main-body">
+        {/*align the header according to the window sizes*/}
+        <div className="heading_align">
+          <div className="app-heading-header content-padding -flex -flex-col">
+            <div className="-mb-30">
+              {/*showing the current location of the page when it navigates from one page to another*/}
+              <CurrentLocation></CurrentLocation>
+            </div>
+            {/*main topic of the heading*/}
+            <h2 className="fancy-heading -no-margin">LOOK WHAT PEOPLE ARE TALKING OF PHONES</h2>
           </div>
-          {/*main topic of the heading*/}
-          <h2 className="fancy-heading -no-margin">LOOK WHAT PEOPLE ARE TALKING OF PHONES...</h2>
+          <div className=" -mt-60 -mb-90 content-padding">
+            <FancyHeading decoratorClassName="fancy-heading2-decorator">
+              <h2 className="heading2 -medium -no-margin heading2-sep-margin">
+                {/*subtopic of the heading*/}
+                Smartphone Feature Trendz
+              </h2>
+            </FancyHeading>
+            {/*line seperator after the subtopic*/}
+            <hr className="heading-sep" />
+          </div>
         </div>
-        <div className=" -mt-60 -mb-90 content-padding">
-          <FancyHeading decoratorClassName="fancy-heading2-decorator">
-            <h2 className="heading2 -medium -no-margin heading2-sep-margin">
-              {/*subtopic of the heading*/}
-              Smartphone Feature Trendz
-            </h2>
-          </FancyHeading>
-          {/*line seperator after the subtopic*/}
-          <hr className="heading-sep" />
-        </div>
-      </div>
 
-      <div className="body-split">
-        <div className="-mt-60">
-          <div
-            className="analytics-container cards-split-Elements -mt-40"
-            style={{ marginTop: '-5%' }}
-          >
-            {/*hidding the left card at the begining of the exkey page which represents the treemap*/}
-            {/*making visible the colurful mobile phone image at the exkey home page*/}
-            <div className="analysing_banner">
-              <img
-                style={{ width: '82%', height: '90%', opacity: '0.8' }}
-                src={colourful_mobilePhone}
-              />
-            </div>
-
-            {/*starting of the card right elements*/}
-
-            {/*hidding the right card at the begining of the exkey page which represents the treemap*/}
-            {/*trend description which is dispplayed on the right side of the begining of the exkey page*/}
-            <div className="trend_description_align">
-              <div className="focus-card focus-info-card -mb-40">
+        <div className="body-split">
+          <div className="-mt-60">
+            <div
+              className="analytics-container cards-split-Elements -mt-40"
+              style={{ marginTop: '-5%' }}
+            >
+              {/*hidding the left card at the begining of the exkey page which represents the treemap*/}
+              {/*making visible the colurful mobile phone image at the exkey home page*/}
+              <div className="analysing_banner">
                 <img
-                  src={lightBulb}
-                  style={{
-                    width: '6%',
-                    height: '1%',
-                    paddingRight: '5%',
-                  }}
-                  alt="tip_bulb"
+                  style={{ width: '82%', height: '90%', opacity: '0.8' }}
+                  src={colourful_mobilePhone}
                 />
-                <div style={{ marginTop: '-4%', marginLeft: '10%' }}>
-                  <span className="-bold -normal">
-                    WHY TREND THIS MUCH IMPORTANT FOR YOU ?<br></br>
-                    <br></br>
-                    <br></br>
-                  </span>
-                </div>
-                <div style={{ marginTop: '-3%', textAlign: 'justify' }}>
-                  Trend analysis will help you to grow your market level by identifying areas of
-                  your product willing to design that are performing well as well as areas that are
-                  not. As a result, it provides useful evidence to help you to make informed
-                  decisions about your long-term plan and ways to future-proof your product.
-                </div>
               </div>
-              <div className="analyze_again">
-                {/*starts a loading process after the button click*/}
-                <Button onClick={routePage} iconSrc={rightArrow} loading={btnLoadingState}>
-                  Start Analysing
-                </Button>
+
+              {/*starting of the card right elements*/}
+
+              {/*hidding the right card at the begining of the exkey page which represents the treemap*/}
+              {/*trend description which is dispplayed on the right side of the begining of the exkey page*/}
+              <div className="trend_description_align">
+                <div className="focus-card focus-info-card -mb-40">
+                  <img
+                    src={lightBulb}
+                    style={{
+                      width: '6%',
+                      height: '1%',
+                      paddingRight: '5%',
+                    }}
+                    alt="tip_bulb"
+                  />
+                  <div style={{ marginTop: '-4%', marginLeft: '10%' }}>
+                    <span className="-bold -normal">
+                      WHY TREND THIS MUCH IMPORTANT FOR YOU ?<br></br>
+                      <br></br>
+                      <br></br>
+                    </span>
+                  </div>
+                  <div style={{ marginTop: '-3%', textAlign: 'justify' }}>
+                    Trend analysis will help you to grow your market level by identifying areas of
+                    your product willing to design that are performing well as well as areas that
+                    are not. As a result, it provides useful evidence to help you to make informed
+                    decisions about your long-term plan and ways to future-proof your product.
+                  </div>
+                </div>
+                <div className="analyze_again">
+                  {/*starts a loading process after the button click*/}
+                  <Button onClick={routePage} iconSrc={rightArrow} loading={btnLoadingState}>
+                    Start Analysing
+                  </Button>
+                </div>
               </div>
             </div>
-
-            {/*analyze again button which do all the necessory action after clicking*/}
-
-            {/* </div> */}
           </div>
         </div>
       </div>
--- web-frontend/src/pages/exkey/ExkeyResults.jsx
+++ web-frontend/src/pages/exkey/ExkeyResults.jsx
@@ -19,8 +19,8 @@ const ExkeyResults = () => {
   useEffect(() => {
     setPageLoading(true);
     setTimeout(() => {
-      setTrend(history.location.stateTrending);
-      setOther(history.location.stateOtherKeywords);
+      setTrend(history.location.state.stateTrending);
+      setOther(history.location.state.stateOtherKeywords);
 
       setPageLoading(false);
     }, 500);
@@ -53,127 +53,129 @@ const ExkeyResults = () => {
   // const { trendingFeatures } = this.state;
   // const history = useHistory();
 
-  // const trendingFeatures = history.location.state.trendList;
+  // const trendingFeatures = history.location.state.stateTrending;
 
-  // const exkeyDataotherKeyword = history.location.state.otherKeywords;
+  // const exkeyDataotherKeyword = history.location.state.stateOtherKeywords;
 
   return (
-    <div className="main-body">
-      {/*align the header according to the window sizes*/}
-      <div className="heading_align">
-        <div className="app-heading-header content-padding -flex -flex-col">
-          <div className="-mb-30">
-            {/*showing the current location of the page when it navigates from one page to another*/}
-            <NavLink to="/analytics/exkey" className="-text-decoration-none">
-              <IconHeading size="extra-small" iconUrl="var(--arrow-back-icon)">
-                <h4 className="heading4 -no-margin">
-                  <span className="header-go-back">Back</span>
-                </h4>
-              </IconHeading>
-            </NavLink>
+    <div className="navbar-page-container -mb-40" style={{ margin: '0%' }}>
+      <div className="main-body">
+        {/*align the header according to the window sizes*/}
+        <div className="heading_align">
+          <div className="app-heading-header content-padding -flex -flex-col">
+            <div className="-mb-30">
+              {/*showing the current location of the page when it navigates from one page to another*/}
+              <NavLink to="/analytics/exkey" className="-text-decoration-none">
+                <IconHeading size="extra-small" iconUrl="var(--arrow-back-icon)">
+                  <h4 className="heading4 -no-margin">
+                    <span className="header-go-back">Back</span>
+                  </h4>
+                </IconHeading>
+              </NavLink>
+            </div>
+            {/*main topic of the heading*/}
+            <h2 className="fancy-heading -no-margin">RESULTS</h2>
           </div>
-          {/*main topic of the heading*/}
-          <h2 className="fancy-heading -no-margin">RESULTS</h2>
+          <div className=" -mt-60 -mb-90 content-padding">
+            <FancyHeading decoratorClassName="fancy-heading2-decorator">
+              <h2 className="heading2 -medium -no-margin heading2-sep-margin">
+                {/*subtopic of the heading*/}
+                Trending Smartphone Features
+              </h2>
+            </FancyHeading>
+            {/*line seperator after the subtopic*/}
+            <hr className="heading-sep" />
+          </div>
         </div>
-        <div className=" -mt-60 -mb-90 content-padding">
-          <FancyHeading decoratorClassName="fancy-heading2-decorator">
-            <h2 className="heading2 -medium -no-margin heading2-sep-margin">
-              {/*subtopic of the heading*/}
-              Trending Smartphone Features
-            </h2>
-          </FancyHeading>
-          {/*line seperator after the subtopic*/}
-          <hr className="heading-sep" />
-        </div>
-      </div>
 
-      {pageLoading ? (
-        <div className="-flex -full-width -mb-70 -mt-40">
-          <div
-            style={{ marginLeft: '48%', marginTop: '10%' }}
-            className=" spinner spinner-medium spinner-accent"
-          ></div>
-        </div>
-      ) : (
-        <div className="body-split">
-          <div className="-mt-60">
-            <div className="analytics-container cards-split -mt-40">
-              {/*hidding the left card at the begining of the exkey page which represents the treemap*/}
-              <div className="card-left">
-                <div className="card-heading-name-left">
-                  <div className="card-topic">
-                    <h3 className="heading3 -medium">
-                      {/*Name of the left card which represents the bargraph of trending keywords*/}
-                      TREND
-                      {/*trending fire image*/}
-                      <img
-                        src={fire}
-                        style={{
-                          width: '5%',
-                          height: '7.2%',
-                          marginLeft: '3%',
-                        }}
-                        alt="tip_bulb"
-                      />
-                    </h3>
-                  </div>
-                </div>
-
-                {/*showing the bargrpah to the user after the relavent timecount mentioned above inside the button action*/}
-                <div className="frequency-bars">
-                  {trendingFeatures.map((item, i) => (
-                    <Bargraph key={i} value={item.value} keyword={item.keyword}></Bargraph>
-                  ))}
-                </div>
-              </div>
-
-              {/*starting of the card right elements*/}
-
-              {/*hidding the right card at the begining of the exkey page which represents the treemap*/}
-
-              <div className="card-right">
-                {/*trend description which is dispplayed on the right side of the begining of the exkey page*/}
-
-                {/*topic which represents the similar keywords of the right card*/}
-                <div className="card-heading-name-right -mb-auto -flex-middle">
-                  <div className="card-topic">
-                    <h3 className="heading3 -medium">SIMILAR KEYWORDS</h3>
-                    <div className="otherKeywords_description">
-                      {/*light bulb and tip for the user displayed with the treemap*/}
-                      <img
-                        src={lightBulb}
-                        style={{
-                          width: '6%',
-                          height: '1%',
-                          marginLeft: '-33%',
-                        }}
-                        alt="tip_bulb"
-                      ></img>
-                      <h4
-                        style={{
-                          height: '1%',
-                          marginLeft: '-25%',
-                          marginTop: '-3%',
-                          width: '110%',
-                          color: ' #bb5959 ',
-                        }}
-                      >
-                        TIP : HOVER OVER THE KEYWORD BOXES TO FIND HOW MUCH THESE KEYWORDS ARE
-                        TRENDING
-                      </h4>
+        {pageLoading ? (
+          <div className="-flex -full-width -mb-70 -mt-40">
+            <div
+              style={{ marginLeft: '48%', marginTop: '10%' }}
+              className=" spinner spinner-medium spinner-accent"
+            ></div>
+          </div>
+        ) : (
+          <div className="body-split">
+            <div className="-mt-60">
+              <div className="analytics-container cards-split -mt-40">
+                {/*hidding the left card at the begining of the exkey page which represents the treemap*/}
+                <div className="card-left">
+                  <div className="card-heading-name-left">
+                    <div className="card-topic">
+                      <h3 className="heading3 -medium">
+                        {/*Name of the left card which represents the bargraph of trending keywords*/}
+                        TREND
+                        {/*trending fire image*/}
+                        <img
+                          src={fire}
+                          style={{
+                            width: '5%',
+                            height: '7.2%',
+                            marginLeft: '3%',
+                          }}
+                          alt="tip_bulb"
+                        />
+                      </h3>
                     </div>
                   </div>
 
-                  {/*Treemap which is imported from the graph folder which represents the similar keywords*/}
-                  <div className="treeMap_align">
-                    <TreeMap data={exkeyDataotherKeyword} />
+                  {/*showing the bargrpah to the user after the relavent timecount mentioned above inside the button action*/}
+                  <div className="frequency-bars">
+                    {trendingFeatures.map((item, i) => (
+                      <Bargraph key={i} value={item.value} keyword={item.keyword}></Bargraph>
+                    ))}
+                  </div>
+                </div>
+
+                {/*starting of the card right elements*/}
+
+                {/*hidding the right card at the begining of the exkey page which represents the treemap*/}
+
+                <div className="card-right">
+                  {/*trend description which is dispplayed on the right side of the begining of the exkey page*/}
+
+                  {/*topic which represents the similar keywords of the right card*/}
+                  <div className="card-heading-name-right -mb-auto -flex-middle">
+                    <div className="card-topic">
+                      <h3 className="heading3 -medium">SIMILAR KEYWORDS</h3>
+                      <div className="otherKeywords_description">
+                        {/*light bulb and tip for the user displayed with the treemap*/}
+                        <img
+                          src={lightBulb}
+                          style={{
+                            width: '6%',
+                            height: '1%',
+                            marginLeft: '-33%',
+                          }}
+                          alt="tip_bulb"
+                        ></img>
+                        <h4
+                          style={{
+                            height: '1%',
+                            marginLeft: '-25%',
+                            marginTop: '-3%',
+                            width: '110%',
+                            color: ' #bb5959 ',
+                          }}
+                        >
+                          TIP : HOVER OVER THE KEYWORD BOXES TO FIND HOW MUCH THESE KEYWORDS ARE
+                          TRENDING
+                        </h4>
+                      </div>
+                    </div>
+
+                    {/*Treemap which is imported from the graph folder which represents the similar keywords*/}
+                    <div className="treeMap_align">
+                      <TreeMap data={exkeyDataotherKeyword} />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
