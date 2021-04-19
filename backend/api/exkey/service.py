7c8ccc15ba1bbc31512ecb8c4d615c42c35e26a7
Manthila <adeesha.2018357@iit.ac.lk>
2021-04-19 22:55:28 +0530

    Response Updated series included

--- backend/api/exkey/service.py
+++ backend/api/exkey/service.py
@@ -1,21 +1,34 @@
-import os 
 import json
 from gensim.models import Word2Vec
 
 keyword_resource =open('analytics/keyword_extract/word_count_ref.json','r')
+#Opening Resources required
 similarity_resource = Word2Vec.load('analytics/wordEmbedding.sav')
 trend_data = json.load(keyword_resource)
+#Calculating total Number of Word Appearance
 totalInstances = sum([x[1] for x in trend_data])
 
 
 
 def calculate_trend_percentage(elementInstances):
+    '''
+        Calculate the percentage of instance given
+    '''
     return elementInstances / totalInstances * 100
 
 def wordEmbedSimilarFunction(candidateKey,numberOfIns):
+    '''
+        Get the similar Words 
+        @candidateKey target word
+        @numberOfIns number of similar word
+    '''
     return similarity_resource.wv.most_similar(candidateKey.lower(),topn=numberOfIns)
 
 def get_trend_word_count(numberOfTrendValues):
+    '''
+        Get the similar Words 
+        @numberOfTrendValues front end requirements     
+    '''
     trend_list =[]
     sample_trend_data = trend_data[0:numberOfTrendValues]
     for element in sample_trend_data:
@@ -23,6 +36,11 @@ def get_trend_word_count(numberOfTrendValues):
     return trend_list
 
 def get_similar_word_instances(topCountWords,numberOfCandidates):
+    '''
+        Get the similar Word list
+        @topCountWords front end requirements (top most instance words)
+        @numberOfCandidates frontend requirement 
+    '''
     similar_ins_list =[]
     cadidate_list = trend_data[0:numberOfCandidates]
     for i in range(0,len(cadidate_list)):
