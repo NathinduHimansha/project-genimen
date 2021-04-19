060f596531553ea94b2da4fbf43775d795d03c96
Manthila <adeesha.2018357@iit.ac.lk>
2021-04-19 19:09:20 +0530

    inital keyword api

---
+++ backend/api/exkey/controller.py
@@ -0,0 +1,28 @@
+from flask import Flask, Blueprint, request
+from api.exkey import service
+#from api.exkey import keyword_service
+#from api.exkey.wordCounter import WordCounter
+
+# Design Specific values
+numberOfTrendValues = 9
+numberOfTopSimilarityWords = 3
+
+exKey = Blueprint('exKey', __name__, url_prefix='/api')
+
+@exKey.route('/Exkey/GetTrends',methods=['GET'])
+def get_bar_graph_values():
+    bargraph ={}
+    valueList = service.get_trend_word_count(numberOfTrendValues)
+    return {'data':{'trend':valueList}}
+
+@exKey.route('/Exkey/GetCandidates',methods=['GET'])
+def get_treeMap_values():
+    treeMap ={'series':[{'data':[]}]}
+
+    similar_word_list = service.get_similar_word_instances(numberOfTopSimilarityWords,numberOfTrendValues)
+    for element in similar_word_list:
+        treeMap['series'][0]['data'].append({'x':element[0],'y':200})
+    
+    return treeMap
+
+
---
+++ backend/api/exkey/service.py
@@ -0,0 +1,37 @@
+import os 
+import json
+from gensim.models import Word2Vec
+
+keyword_resource =open('analytics/keyword_extract/word_count_ref.json','r')
+similarity_resource = Word2Vec.load('analytics/wordEmbedding.sav')
+trend_data = json.load(keyword_resource)
+totalInstances = sum([x[1] for x in trend_data])
+
+
+
+def calculate_trend_percentage(elementInstances):
+    return elementInstances / totalInstances * 100
+
+def wordEmbedSimilarFunction(candidateKey,numberOfIns):
+    return similarity_resource.wv.most_similar(candidateKey.lower(),topn=numberOfIns)
+
+def get_trend_word_count(numberOfTrendValues):
+    trend_list =[]
+    sample_trend_data = trend_data[0:numberOfTrendValues]
+    for element in trend_data:
+        trend_list.append({'keyword':element[0],'value':calculate_trend_percentage(element[1])})
+    return trend_list
+
+def get_similar_word_instances(topCountWords,numberOfCandidates):
+    similar_ins_list =[]
+    cadidate_list = trend_data[0:numberOfCandidates]
+    for i in range(0,len(cadidate_list)):
+        if i < topCountWords:
+            top_similar_words = wordEmbedSimilarFunction(cadidate_list[i][0],2)
+            similar_ins_list.append(top_similar_words[0][0])
+            similar_ins_list.append(top_similar_words[1][0])
+        else:
+            similar_ins_list.append(wordEmbedSimilarFunction(cadidate_list[i][0],1)[0])
+    return similar_ins_list
+        
+
