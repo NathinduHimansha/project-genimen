b8223e9b059cee85e1ee9866afcb3723f5ee9da6
bsucker98 <bmorashad@gmail.com>
2021-04-20 17:44:00 +0530

    minor bug fix

--- web-frontend/src/pages/pssa/PssaView.jsx
+++ web-frontend/src/pages/pssa/PssaView.jsx
@@ -123,7 +123,7 @@ const Pssa3View = () => {
         <div className="content-padding -flex">
           <div style={{ width: '50%' }} className="feature-selection-box">
             <div className="focus-card focus-info-card -mb-40">
-              <div className="-bold -normal focus-card-info-lable">Info: </div>
+              <div className="-bold -normal focus-card-info-label">Info: </div>
               <div className="focus-card-description">
                 Select the smart phone you want to analyse and get a insight from. This will give
                 you the sentiment of the features of the selected smart phones and an overall score
