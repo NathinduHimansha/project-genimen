d5258085fdf0599bd04b1ae9074ea349fdd3cafe
bsucker98 <bmorashad@gmail.com>
2021-04-23 00:44:23 +0530

    first release cleanup

--- Frequency Based feature extraction/initializeSimilarWord.py
+++
@@ -1,9 +0,0 @@
-# Saving the instance with pickle
-from similarWord import SimilarWord,Word2Vec
-import pickle
-similarWordVec = SimilarWord ('customer_reviews.txt')
-similarWordVec.buildVec()
-
-pickle.dump(similarWordVec,open('similarWord.pkl','wb'))
-# testing statement
-# print(similarWordVec.get_most_similar('SCREEN',10))
